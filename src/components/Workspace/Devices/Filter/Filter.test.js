6ede93c4cc43f39bc5d9f199a208fd5511667eda
rene_mbp <harryloveslearning@googlemail.com>
2018-10-03 11:31:57 +0200

    add filter tests

---
+++ src/components/Workspace/Devices/Filter/Filter.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import Filter from "./index.js";
+
+it("renders without crashing", () => {
+  shallow(<Filter />);
+});
+
+it("calls onSubmit on return", () => {
+  const submitMock = jest.fn();
+  const changeMock = jest.fn();
+  const wrapper = mount(<Filter onSubmit={submitMock} onChange={changeMock} />);
+
+  const input = wrapper.find("input");
+  input.simulate("change", { target: { value: "abc" } });
+  wrapper.find("form").simulate("submit");
+
+  expect(changeMock).toHaveBeenCalledWith("abc");
+  expect(submitMock).toHaveBeenCalledWith("abc");
+});
+
+it("calls onSubmit on clear input", () => {
+  const submitMock = jest.fn();
+  const changeMock = jest.fn();
+  const wrapper = mount(<Filter onSubmit={submitMock} onChange={changeMock} />);
+
+  const input = wrapper.find("input");
+  input.simulate("change", { target: { value: "abc" } });
+  wrapper.find("button").simulate("click");
+  expect(submitMock).toHaveBeenCalledWith("");
+});
