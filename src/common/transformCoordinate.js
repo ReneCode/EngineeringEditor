de3b3bb03fa0233c784329b97abf403d9314be5e
rene_mbp <harryloveslearning@googlemail.com>
2018-10-29 21:04:30 +0100

    transform canvasToWc and wcToCanvas running

--- src/common/transformCoordinate.js
+++ src/common/transformCoordinate.js
@@ -1,3 +1,5 @@
+import Point from "./Point";
+
 class TransformCoordinate {
   constructor(viewport, canvas) {
     //  adapt viewport to get vieport and canvas fit together
@@ -8,19 +10,33 @@ class TransformCoordinate {
     if (ratioX > ratioY) {
       // keep height of viewport / keep ratioY
       // strech x to both sides
+      this.scale = ratioY;
       const oldVpWidth = this.viewport.width;
       this.viewport.width = this.canvas.width / ratioY;
       this.viewport.x -= (this.viewport.width - oldVpWidth) / 2;
     } else {
       // keep width of viewport
       // stretch height and center it
+      this.scale = ratioX;
       const oldVpHeight = this.viewport.height;
       this.viewport.height = this.canvas.height / ratioX;
       this.viewport.y -= (this.viewport.height - oldVpHeight) / 2;
     }
   }
 
-  wcToScreen(pt) {}
+  wcToCanvas(pt) {
+    const x = (pt.x - this.viewport.x) * this.scale;
+    const y =
+      this.canvas.height - (pt.y - this.viewport.y) * this.scale;
+    return new Point(x, y);
+  }
+
+  canvasToWc(pt) {
+    const x = pt.x / this.scale + this.viewport.x;
+    const y =
+      (this.canvas.height - pt.y) / this.scale + this.viewport.y;
+    return new Point(x, y);
+  }
 }
 
 export default TransformCoordinate;
--- src/common/transformCoordinate.test.js
+++ src/common/transformCoordinate.test.js
@@ -1,22 +1,46 @@
 import TransformCoordinate from "./transformCoordinate";
+import Point from "./Point";
 
 describe("transformCoordinate", () => {
-  it("adapt viewport -  stretch viewport X", () => {
-    const viewport = {
-      x: 100,
-      y: 200,
-      width: 10,
-      height: 30,
-    };
-    const canvas = {
-      width: 400,
-      height: 300,
-    };
-    const tc = new TransformCoordinate(viewport, canvas);
-    expect(tc.viewport.height).toBe(30);
-    expect(tc.viewport.width).toBe(40);
-    expect(tc.viewport.x).toBe(85);
-    expect(tc.viewport.y).toBe(200);
+  describe("vieport has to be strechted", () => {
+    let tc = undefined;
+    beforeEach(() => {
+      const viewport = {
+        x: 100,
+        y: 200,
+        width: 10,
+        height: 30,
+      };
+      const canvas = {
+        width: 400,
+        height: 300,
+      };
+      tc = new TransformCoordinate(viewport, canvas);
+    });
+
+    it("adapt viewport -  stretch viewport X", () => {
+      expect(tc.viewport.height).toBe(30);
+      expect(tc.viewport.width).toBe(40);
+      expect(tc.viewport.x).toBe(85);
+      expect(tc.viewport.y).toBe(200);
+      expect(tc.scale).toBe(10);
+    });
+    it("wcToCanvas a", () => {
+      const pt = new Point(105, 205);
+      expect(tc.wcToCanvas(pt)).toEqual(new Point(200, 250));
+    });
+    it("wcToCanvas b", () => {
+      const pt = new Point(110, 230);
+      expect(tc.wcToCanvas(pt)).toEqual(new Point(250, 0));
+    });
+    it("canvasToWc a", () => {
+      const pt = new Point(200, 250);
+      expect(tc.canvasToWc(pt)).toEqual(new Point(105, 205));
+    });
+    it("canvasToWc b", () => {
+      const pt = new Point(250, 0);
+      expect(tc.canvasToWc(pt)).toEqual(new Point(110, 230));
+    });
   });
 
   it("adapt viewport -  stretch viewport Y", () => {
@@ -35,5 +59,6 @@ describe("transformCoordinate", () => {
     expect(tc.viewport.width).toBe(40);
     expect(tc.viewport.x).toBe(100);
     expect(tc.viewport.y).toBe(190);
+    expect(tc.scale).toBe(10);
   });
 });
