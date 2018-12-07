473132c4748e2ee18ac029e57a5ad8128a929f1d
rene_mbp <harryloveslearning@googlemail.com>
2018-12-07 16:28:04 +0100

    with AutoConnect Lines

--- src/components/Autoconnection.tsx
+++ src/components/Autoconnection.tsx
@@ -2,155 +2,25 @@ import { Component } from "react";
 import { connect } from "react-redux";
 import { IGlobalState } from "../reducers";
 import Placement from "../model/Placement";
-import GraphicSymbolRef from "../model/graphic/GraphicSymbolRef";
-import GraphicSymbol from "../model/graphic/GraphicSymbol";
-import { symbol } from "prop-types";
-import GraphicConnectionPoint, {
-  ConnectionPointDirection,
-} from "../model/graphic/GraphicConnectionPoint";
-import Point from "../common/point";
-import { channel } from "redux-saga";
+import AutoConnectionUtil from "../model/AutoConnectionUtil";
+import GraphicLine from "../model/graphic/GraphicLine";
+import * as actions from "../actions";
 
 interface IProps {
   items: Placement[];
-}
-
-interface IPlacementAndConnectionPoint {
-  placement: Placement;
-  index: number;
-  pt: Point;
-  direction: ConnectionPointDirection;
+  dispatch: Function;
 }
 
 class Autoconnection extends Component<IProps> {
   componentDidUpdate() {
-    const reduceSymbolRefToCpWithSymbolRef = (
-      acc: IPlacementAndConnectionPoint[],
-      s: { placement: Placement; symbolRef: GraphicSymbolRef },
-    ): IPlacementAndConnectionPoint[] => {
-      if (s.symbolRef && s.symbolRef.symbol) {
-        const symbol = s.symbolRef.symbol;
-        const symbolPt = s.symbolRef.pt.sub(symbol.insertPt);
-        // console.log(
-        //   s.symbolRef.pt,
-        //   symbol.insertPt,
-        //   symbolPt,
-        //   symbol.items,
-        // );
-        const cpsWithSymbolRef = symbol.items
-          .filter(g => g.type === "connectionpoint")
-          .map(g => g as GraphicConnectionPoint)
-          .map((cp, index) => {
-            return {
-              placement: s.placement,
-              index: index,
-              pt: symbolPt.add(cp.pt),
-              direction: cp.direction,
-            };
-          });
-        return acc.concat(cpsWithSymbolRef);
-      } else {
-        return acc;
-      }
-    };
+    const ac = new AutoConnectionUtil(this.props.items);
+    const pairs = ac.getConnectionPairs();
 
-    const sortConnectionPoints = (
-      a: IPlacementAndConnectionPoint,
-      b: IPlacementAndConnectionPoint,
-    ) => {
-      if (
-        (a.direction === ConnectionPointDirection.DOWN ||
-          a.direction === ConnectionPointDirection.UP) &&
-        (b.direction === ConnectionPointDirection.LEFT ||
-          b.direction === ConnectionPointDirection.RIGHT)
-      ) {
-        return 1;
-      }
-      if (
-        (a.direction === ConnectionPointDirection.LEFT ||
-          a.direction === ConnectionPointDirection.RIGHT) &&
-        (b.direction === ConnectionPointDirection.UP ||
-          b.direction === ConnectionPointDirection.DOWN)
-      ) {
-        return -1;
-      }
-      // direction fits
-      if (
-        a.direction === ConnectionPointDirection.LEFT ||
-        a.direction === ConnectionPointDirection.RIGHT
-      ) {
-        // left-right direction
-
-        // 1. order by y-coordinate
-        if (a.pt.y < b.pt.y) {
-          return -1;
-        }
-        if (a.pt.y > b.pt.y) {
-          return 1;
-        }
-        // 1.order by x-coordinate
-        if (a.pt.x < b.pt.x) {
-          return -1;
-        } else {
-          return 1;
-        }
-      } else {
-        // UP-DOWN direction
-
-        // 1. order by x-coordinate
-        if (a.pt.x < b.pt.x) {
-          return -1;
-        }
-        if (a.pt.x > b.pt.x) {
-          return 1;
-        }
-        // 1.order by y-coordinate
-        if (a.pt.y < b.pt.y) {
-          return -1;
-        } else {
-          return 1;
-        }
-      }
-      return 1;
-    };
-
-    const cps = this.props.items
-      .map(p => {
-        // console.log((p.graphic as GraphicSymbolRef).pt);
-        return { placement: p, graphic: p.graphic };
-      })
-      .filter((s: any) => s.graphic.type === "symbolref")
-      .map((s: any) => {
-        return {
-          placement: s.placement,
-          symbolRef: s.graphic as GraphicSymbolRef,
-        };
-      })
-      .reduce(reduceSymbolRefToCpWithSymbolRef, [])
-      .sort(sortConnectionPoints)
-      .map((s: IPlacementAndConnectionPoint) => ({
-        direction: s.direction,
-        pt: s.pt,
-      }));
-
-    // console.log("cps: ", JSON.stringify(cps, null, 2));
-
-    // const len = connectionPoints.length;
-    // for (let i = 0; i < len; i++) {
-    //   const dirA = connectionPoints[i].direction;
-    //   for (let j = i + 1; j < len; j++) {
-    //     const dirB = connectionPoints[j].direction;
-    //   }
-    // }
-
-    //   (acc, s) =>
-    //     acc.concat(
-    //       (s as GraphicSymbol).items.map(
-    //         g => g.type === "connectionpoint",
-    //       ),
-    //     ),
-    //   [],
-    // );
+    const lines = pairs.map(p => {
+      const line = new GraphicLine(p.source.pt, p.dest.pt);
+      return line;
+    });
+    this.props.dispatch(actions.setTempItem(lines));
   }
 
   render() {
---
+++ src/model/AutoConnectionUtil.ts
@@ -0,0 +1,172 @@
+import Placement from "./Placement";
+import Point from "../common/point";
+import GraphicConnectionPoint, {
+  ConnectionPointDirection,
+} from "./graphic/GraphicConnectionPoint";
+import GraphicSymbolRef from "./graphic/GraphicSymbolRef";
+
+interface IPlacementAndConnectionPoint {
+  placement: Placement;
+  index: number;
+  pt: Point;
+  direction: ConnectionPointDirection;
+}
+
+interface IPlacementAndConnectionPointPair {
+  source: IPlacementAndConnectionPoint;
+  dest: IPlacementAndConnectionPoint;
+}
+
+class AutoConnectionUtil {
+  placements: Placement[];
+
+  constructor(placements: Placement[]) {
+    this.placements = placements;
+  }
+
+  getConnectionPairs(): IPlacementAndConnectionPointPair[] {
+    const connectionPoints = this.getSortedConnectionPoints();
+
+    const pairs = [];
+    const len = connectionPoints.length;
+    let i = 0;
+    while (i < len - 1) {
+      const source = connectionPoints[i];
+      const iDest = i + 1;
+      const dest = connectionPoints[iDest];
+      if (this.match(source, dest)) {
+        pairs.push({
+          source: connectionPoints[i],
+          dest: connectionPoints[iDest],
+        });
+        i += 2;
+      } else {
+        i++;
+      }
+    }
+    return pairs;
+  }
+
+  match(
+    source: IPlacementAndConnectionPoint,
+    dest: IPlacementAndConnectionPoint,
+  ): boolean {
+    if (
+      source.direction === ConnectionPointDirection.UP &&
+      dest.direction === ConnectionPointDirection.DOWN &&
+      source.pt.x === dest.pt.x
+    ) {
+      return true;
+    }
+
+    if (
+      source.direction === ConnectionPointDirection.RIGHT &&
+      dest.direction === ConnectionPointDirection.LEFT &&
+      source.pt.y === dest.pt.y
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  getSortedConnectionPoints() {
+    return this.placements
+      .map(p => {
+        return { placement: p, graphic: p.graphic };
+      })
+      .filter((s: any) => s.graphic.type === "symbolref")
+      .map((s: any) => {
+        return {
+          placement: s.placement,
+          symbolRef: s.graphic as GraphicSymbolRef,
+        };
+      })
+      .reduce(this.reduceSymbolRefToCpWithSymbolRef, [])
+      .sort(this.sortConnectionPoints);
+  }
+
+  private reduceSymbolRefToCpWithSymbolRef = (
+    acc: IPlacementAndConnectionPoint[],
+    s: { placement: Placement; symbolRef: GraphicSymbolRef },
+  ): IPlacementAndConnectionPoint[] => {
+    if (s.symbolRef && s.symbolRef.symbol) {
+      const symbol = s.symbolRef.symbol;
+      const symbolPt = s.symbolRef.pt.sub(symbol.insertPt);
+      const cpsWithSymbolRef = symbol.items
+        .filter(g => g.type === "connectionpoint")
+        .map(g => g as GraphicConnectionPoint)
+        .map((cp, index) => {
+          return {
+            placement: s.placement,
+            index: index,
+            pt: symbolPt.add(cp.pt),
+            direction: cp.direction,
+          };
+        });
+      return acc.concat(cpsWithSymbolRef);
+    } else {
+      return acc;
+    }
+  };
+  private sortConnectionPoints = (
+    a: IPlacementAndConnectionPoint,
+    b: IPlacementAndConnectionPoint,
+  ) => {
+    if (
+      (a.direction === ConnectionPointDirection.DOWN ||
+        a.direction === ConnectionPointDirection.UP) &&
+      (b.direction === ConnectionPointDirection.LEFT ||
+        b.direction === ConnectionPointDirection.RIGHT)
+    ) {
+      return 1;
+    }
+    if (
+      (a.direction === ConnectionPointDirection.LEFT ||
+        a.direction === ConnectionPointDirection.RIGHT) &&
+      (b.direction === ConnectionPointDirection.UP ||
+        b.direction === ConnectionPointDirection.DOWN)
+    ) {
+      return -1;
+    }
+    // direction fits
+    if (
+      a.direction === ConnectionPointDirection.LEFT ||
+      a.direction === ConnectionPointDirection.RIGHT
+    ) {
+      // left-right direction
+
+      // 1. order by y-coordinate
+      if (a.pt.y < b.pt.y) {
+        return -1;
+      }
+      if (a.pt.y > b.pt.y) {
+        return 1;
+      }
+      // 1.order by x-coordinate
+      if (a.pt.x < b.pt.x) {
+        return -1;
+      } else {
+        return 1;
+      }
+    } else {
+      // UP-DOWN direction
+
+      // 1. order by x-coordinate
+      if (a.pt.x < b.pt.x) {
+        return -1;
+      }
+      if (a.pt.x > b.pt.x) {
+        return 1;
+      }
+      // 1.order by y-coordinate
+      if (a.pt.y < b.pt.y) {
+        return -1;
+      } else {
+        return 1;
+      }
+    }
+    return 1;
+  };
+}
+
+export default AutoConnectionUtil;
