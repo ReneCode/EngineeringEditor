c62d3ca1a321c382bdd6f671a9214d57fea23399
rene_mbp <harryloveslearning@googlemail.com>
2018-12-17 22:07:54 +0100

    fix placement.test

--- src/model/Placement.test.ts
+++ src/model/Placement.test.ts
@@ -1,16 +1,16 @@
-import PlacementBase from "./Placement";
 import GraphicBase from "./graphic/GraphicBase";
 import Point from "../common/point";
 import GraphicLine from "./graphic/GraphicLine";
 import Placement from "./Placement";
+import GraphicCircle from "./graphic/GraphicCircle";
 
 describe("placement", () => {
   const projectId = "projectId";
   const pageId = "pageId";
 
-  it("test with graphicBase", () => {
-    const graphic = new GraphicBase("line");
-    const placement = new PlacementBase(projectId, pageId, graphic);
+  it("test with GraphicCircle", () => {
+    const graphic = new GraphicCircle(new Point(2, 3), 10);
+    const placement = new Placement(projectId, pageId, graphic);
     expect(placement.graphic).toBe(graphic);
     expect(typeof placement.graphic).toBe("object");
 
@@ -18,11 +18,19 @@ describe("placement", () => {
     expect(typeof json).toBe("object");
     expect(json).toHaveProperty("graphic");
     expect(typeof json.graphic).toBe("string");
+
+    const newPlacement = Placement.fromDTO(json) as Placement;
+    expect(newPlacement).toBeTruthy();
+    expect(
+      newPlacement.graphic instanceof GraphicCircle,
+    ).toBeTruthy();
+    const circle = newPlacement.graphic as GraphicCircle;
+    expect(circle.pt).toEqual(graphic.pt);
   });
 
   it("test with GraphicLine", () => {
     const graphic = new GraphicLine(new Point(1, 2), new Point(4, 5));
-    const placement = new PlacementBase(projectId, pageId, graphic);
+    const placement = new Placement(projectId, pageId, graphic);
     placement.id = "42";
     expect(placement.graphic).toBe(graphic);
     expect(typeof placement.graphic).toBe("object");
